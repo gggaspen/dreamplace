2cf46fe6d935480a60bb031b9339ce551c3f5fe6
gggaspen <gggaspen@gmail.com>
2024-11-13 20:18:46 -0300

    carousell

--- src/components/carousel-v2/Carousel.tsx
+++ src/components/carousel-v2/Carousel.tsx
@@ -7,29 +7,31 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/pagination";
-import { Box } from "@chakra-ui/react";
 import "./Carousel.css";
 
 const images = [
-  "https://i.postimg.cc/2SFKfTPn/Runa-4.jpg",
-  "https://i.postimg.cc/7PT33YMf/AGUS4.jpg",
-  "https://i.postimg.cc/NFTRBpf1/DSC-2471.jpg",
-  "https://i.postimg.cc/4NkzBkBw/UY.jpg",
-  // "/src/assets/img/banner-1.png",
+  "https://i.postimg.cc/j5vtVgdF/Banner-Carrusel-1.png",
+  "https://i.postimg.cc/KYPmfvXF/Banner-Carrusel-2.png",
+  "https://i.postimg.cc/QCtsjQFx/Banner-Carrusel-3.png",
+  "https://i.postimg.cc/TwTf4MGb/Banner-Carrusel-4.png",
+  // "https://i.postimg.cc/2SFKfTPn/Runa-4.jpg",
+  // "https://i.postimg.cc/7PT33YMf/AGUS4.jpg",
+  // "https://i.postimg.cc/NFTRBpf1/DSC-2471.jpg",
+  // "https://i.postimg.cc/4NkzBkBw/UY.jpg",
 ];
 
 export default function CarrousselV2() {
   return (
     <>
-      <div className="w-full relative pt-4">
+      <div className="w-full relative">
         <Swiper
           className="progress-slide-carousel"
           modules={[Autoplay, Pagination]}
           loop={true}
-          // autoplay={{
-          //   delay: 1200,
-          //   disableOnInteraction: false,
-          // }}
+          autoplay={{
+            delay: 1200,
+            disableOnInteraction: false,
+          }}
           pagination={{
             clickable: true,
             type: "progressbar",
@@ -39,10 +41,10 @@ export default function CarrousselV2() {
           {images.map((image, index) => (
             <SwiperSlide key={index}>
               <div className="flex justify-center">
-              {/* <div className="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center"> */}
+                {/* <div className="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center"> */}
                 <Image
-                  width={500}
-                  height={500}
+                  width={1000}
+                  height={300}
                   alt={`Image ${index + 1}`}
                   src={image}
                   className=""
--- src/components/iframe/Iframe.tsx
+++ src/components/iframe/Iframe.tsx
@@ -4,15 +4,16 @@ export const Iframe = () => {
   return (
     <Flex
       w={"100%"}
-      // h={"100%"}
-      px={"2em"}
+      h={"50dvw"}
+      p={"4em"}
       justifyContent={"center"}
-      alignItems={"center"}
+      alignItems={"flex-end"}
       // position="absolute"
+      bgImage={"url(https://i.postimg.cc/fWtDqKQB/Banner-Prensa.png)"}
     >
       <iframe
         style={{ borderRadius: "14px" }}
-        src="https://open.spotify.com/embed/artist/6dd2fVevgttSYrLvsRqdTI?utm_source=generator&theme=0"
+        src="https://open.spotify.com/embed/album/5qU46hoQWsXz3Ciw6p3Cgt?utm_source=generator&theme=0"
         width="100%"
         height="152"
         // frameBorder="0"
