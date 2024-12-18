87bf9115611b8a9ce11172c8028b2a25ec1411ca
gggaspen <gggaspen@gmail.com>
2024-12-18 15:28:51 -0300

    images

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,19 @@ const nextConfig = {
     optimizePackageImports: ["@chakra-ui/react"],
   },
   images: {
+    // disableStaticImages: true, // Deshabilita la optimización de imágenes
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.postimg.cc",
         pathname: "/**",
       },
+    ],
+    domains: [
+      "localhost",
+      "dreamplace.com.ar",
+      "https://dreamplace-production.up.railway.app/",
+      "i.postimg.cc",
     ],
   },
   output: "standalone",
