edbcfa8764aeccb9b1451e22e884e5ec3c4beeae
gggaspen <gggaspen@gmail.com>
2025-01-17 17:29:51 -0300

    loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Press from "@/components/press/Press";
 import Footer from "@/components/footer/Footer";
 import { fetchAllData } from "@/services/data.service"; // Centralizamos las llamadas a servicios
 import { dateToCustomString } from "@/utils/format-date";
+import LoadingScreen from "@/components/loading-screen/LoadingScreen";
 
 export default function Home() {
   const [data, setData] = useState<any>(null);
@@ -31,7 +32,9 @@ export default function Home() {
     fetchData();
   }, []);
 
-  if (!data) return <React.Fragment>Loading...</React.Fragment>;
+  if (!data) {
+    return <LoadingScreen />;
+  }
 
   const {
     // events,
